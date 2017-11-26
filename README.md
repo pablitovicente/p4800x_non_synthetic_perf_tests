32f0c926ade0342e03a841d7fa9779e1e5689e50
Pablo Vicente <pvicente@gmail.com>
2017-11-26 13:54:34 -0300

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+Requesites 
+
+* Mysql 14.14+
+* Nodejs v8.9.1
+* ap (Apache Bench)
+* Redis
+
---
+++ sql_test.js
@@ -0,0 +1,54 @@
+const mysql = require('mysql');
+const express = require('express');
+
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'test1234',
+  database : 'employees'
+});
+
+connection.connect();
+const app = express();
+
+// Employee ids begin at 10001 and end in 499999, using a mechanical disk was not possible to get many results so this variable is used for tunning how insane this tests is....
+const biggestEmployeeNumber = 11001;
+let requestsServerd = 0;
+
+app.get('/insane', (req, res) => {
+  connection.query(`SELECT * FROM
+    employees.employees e
+      INNER JOIN
+    employees.salaries s ON (e.emp_no = s.emp_no) WHERE e.emp_no <= ${biggestEmployeeNumber} ORDER BY RAND()`, function (error, results, fields) {
+    if (error) throw error;
+    console.log(`sending ${results.length} results to http client...`);
+    requestsServerd++;
+    res.send(results);
+  });
+});
+
+app.get('/massive', (req, res) => {
+  connection.query(`SELECT * FROM
+    employees.employees e
+      INNER JOIN
+    employees.salaries s ON (e.emp_no = s.emp_no) WHERE e.emp_no <= ${biggestEmployeeNumber}`, function (error, results, fields) {
+    if (error) throw error;
+    console.log(`sending ${results.length} results to http client...`);
+    requestsServerd++;
+    res.send(results);
+  });
+});
+
+
+app.get('/serverstats', (req, res) => {
+  const used = process.memoryUsage();
+  const stats = {};
+  for (let key in used) {
+    stats[key] = `${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`;
+  }
+  stats.requestsServerd = requestsServerd;
+  res.send(stats);
+});
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
