c591f29e5e6d80e46c8b6e26f9e0295e907301cb
Pablo Vicente <pvicente@gmail.com>
2017-11-26 14:45:39 -0300

    misc

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ Requesites
 * Mysql 14.14+
 * Nodejs v8.9.1
 * ap (Apache Bench)
-* Redis
+* Redis (sudo add-apt-repository ppa:chris-lea/redis-server && sudo apt install redis-server)
 
--- sql_test.js
+++ sql_test.js
@@ -12,7 +12,7 @@ connection.connect();
 const app = express();
 
 // Employee ids begin at 10001 and end in 499999, using a mechanical disk was not possible to get many results so this variable is used for tunning how insane this tests is....
-const biggestEmployeeNumber = 11001;
+const biggestEmployeeNumber = 10501;
 let requestsServerd = 0;
 
 app.get('/insane', (req, res) => {
@@ -22,7 +22,8 @@ app.get('/insane', (req, res) => {
     employees.salaries s ON (e.emp_no = s.emp_no) WHERE e.emp_no <= ${biggestEmployeeNumber} ORDER BY RAND()`, function (error, results, fields) {
     if (error) throw error;
     console.log(`sending ${results.length} results to http client...`);
-    requestsServerd++;
+    // No time to do stats in Redis...
+    // requestsServerd++;
     res.send(results);
   });
 });
@@ -34,11 +35,25 @@ app.get('/massive', (req, res) => {
     employees.salaries s ON (e.emp_no = s.emp_no) WHERE e.emp_no <= ${biggestEmployeeNumber}`, function (error, results, fields) {
     if (error) throw error;
     console.log(`sending ${results.length} results to http client...`);
-    requestsServerd++;
+    // No time to do stats in Redis...
+    // requestsServerd++;
     res.send(results);
   });
 });
 
+
+app.get('/sample_response_body', (req, res) => {
+  connection.query(`SELECT * FROM
+    employees.employees e
+      INNER JOIN
+    employees.salaries s ON (e.emp_no = s.emp_no) LIMIT 1`, function (error, results, fields) {
+    if (error) throw error;
+    console.log(`sending ${results.length} results to http client...`);
+    // No time to do stats in Redis...
+    // requestsServerd++;
+    res.send(results);
+  });
+});
 
 app.get('/serverstats', (req, res) => {
   const used = process.memoryUsage();
@@ -46,7 +61,8 @@ app.get('/serverstats', (req, res) => {
   for (let key in used) {
     stats[key] = `${Math.round(used[key] / 1024 / 1024 * 100) / 100} MB`;
   }
-  stats.requestsServerd = requestsServerd;
+  // No time to do stats in Redis...
+  // stats.requestsServerd = requestsServerd;
   res.send(stats);
 });
 
